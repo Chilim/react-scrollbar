b4539ea429eb721f30c0c508b2d28d2ee389685b
davud <davud.kakhriman@gmail.com>
2021-01-03 21:26:42 +0100

    rename mock function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import Scrollbar from "./Scrollbar";
+import Scrollbar from './Scrollbar';
 import './App.css';
-import {genList} from '../utils/data';
+import { genData } from '../utils/data';
 
-const data = genList();
+const data = genData();
 
 function App() {
-  return (
-    <div className="App">
-      <Scrollbar rowLength={30} viewHeight={500} viewWidth={700}>
-        {data.map((el, idx) => <div key={idx}>{el}</div>)}
-      </Scrollbar>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Scrollbar rowLength={30} viewHeight={500} viewWidth={700}>
+				{data.map((el, idx) => (
+					<div key={idx}>{el}</div>
+				))}
+			</Scrollbar>
+		</div>
+	);
 }
 
 export default App;
